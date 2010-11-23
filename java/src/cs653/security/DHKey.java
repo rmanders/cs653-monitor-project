96a14df6b0e5907f75841a336c14c7c6f6b6207b
Ryan <Ryan@localhost>
2010-11-22 21:58:26 -0500

    adding security stuff

---
+++ java/src/cs653/security/DHKey.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs653.security;
+
+/**
+ *
+ * @author rmanders
+ */
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Date;
+import java.math.BigInteger;
+
+
+/*
+ * This object is used for Public Key Exchange.
+ * The Crypto routines require it.  I haven't put the heavy
+ * duty methods in here because I want it to stay small
+ */
+
+class DHKey implements Serializable {
+
+    /* These two make up the public Key */
+    private BigInteger p;
+    private BigInteger g;
+    private String description;
+    private Date created;
+
+    // <editor-fold defaultstate="collapsed" desc="Constructor(1)">
+    DHKey(BigInteger p, BigInteger g, String description) {
+        this.p = p;
+        this.g = g;
+        this.description = description;
+        this.created = new Date();
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="getInflatedInstance">
+    /**
+     * Use this static method to load(re-inflate) the DHKey object from a file
+     *
+     * @param filename The filename of the flattened object
+     * @return {@link DHKey} Object
+     */
+    public static DHKey getInflatedInstance(final String filename) {
+        try {
+            FileInputStream fis = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fis);
+            DHKey dhkey = (DHKey) in.readObject();
+            in.close();
+            return dhkey;
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+    // </editor-fold>
+   
+    // <editor-fold defaultstate="collapsed" desc="toString">
+    @Override
+    public String toString() {
+        StringBuilder scratch = new StringBuilder();
+        scratch.append("Public Key(p): ").append(p.toString(32)).append("\n");
+        scratch.append("Public Key(g): ").append(g.toString(32)).append("\n");
+        scratch.append("Description: ").append(description).append("\n");
+        scratch.append("Created: ").append(created);
+        return scratch.toString();
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Default Get Methods">
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigInteger getG() {
+        return g;
+    }
+
+    public BigInteger getP() {
+        return p;
+    }
+    // </editor-fold>
+
+}
+
---
+++ java/src/cs653/security/GenerateDHKeyFile.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs653.security;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+
+/**
+ *
+ * @author rmanders
+ */
+public class GenerateDHKeyFile {
+
+   public static void main (String arg[]) {
+      try {
+	 BigInteger p = new BigInteger("7897383601534681724700886135766287333879367007236994792380151951185032550914983506148400098806010880449684316518296830583436041101740143835597057941064647");
+	 BigInteger g = new BigInteger("2333938645766150615511255943169694097469294538730577330470365230748185729160097289200390738424346682521059501689463393405180773510126708477896062227281603");
+	 DHKey key = new DHKey(p,g,"C653 DH key");
+	 FileOutputStream fos = new FileOutputStream("DHKey");
+	 ObjectOutputStream oos = new ObjectOutputStream(fos);
+	 oos.writeObject(key);
+      } catch (Exception e) {
+	 System.out.println("Whoops!");
+      }
+   }
+   
+}
