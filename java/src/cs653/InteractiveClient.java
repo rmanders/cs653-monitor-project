98ff01a53d7afa0b6f2a9f8f419849703211b06b
Ryan <Ryan@localhost>
2010-11-29 20:55:20 -0500

    interactive client

--- java/src/cs653/InteractiveClient.java
+++ java/src/cs653/InteractiveClient.java
@@ -6,6 +6,7 @@
 package cs653;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -22,6 +23,10 @@ public class InteractiveClient  {
         String inline = null;
         boolean loop = true;
         Scanner scanIn = new Scanner(System.in);
+        ActiveClient client =
+                new ActiveClient("localhost",8150,8250,"andersr9","password");
+        client.openConnection("localhost", 8150);
+        client.login();
 
         while( loop ==  true) {
             inline = scanIn.nextLine();
@@ -29,10 +34,62 @@ public class InteractiveClient  {
             if( inline.trim().equals("quit")) {
                 loop = false;
             } else {
-                System.out.println(">" + inline);
+                StringTokenizer toke = new StringTokenizer(inline," ");
+                if( toke.hasMoreTokens() ) {
+                    Command cmd = makeCommand(toke.nextToken());
+                    if( null == cmd ) {
+                        System.out.println("Invalid Command");
+                        continue;
+                    }
+                    String bargs[] = new String[toke.countTokens()];
+                    int i = 0;
+                    while( toke.hasMoreTokens() ) {
+                        bargs[i] = toke.nextToken();
+                        i++;
+                    }
+                    client.executeCommand(cmd, bargs);
+                    MessageGroup msgs = client.receiveMessageGroup();
+                    System.out.println(msgs);
+                }
             }
 
         } // End while
+        client.closeConnection();
     }
 
+    public static Command makeCommand( String cmd ) {
+        if( cmd.equals("IDENT") ) {
+            return Command.IDENT;
+        }
+        else if (cmd.equals("QUIT")) {
+            return Command.QUIT;
+        }
+        else if (cmd.equals("PASSWORD")) {
+            return Command.PASSWORD;
+        }
+        else if (cmd.equals("CHANGE_PASSWORD")) {
+            return Command.CHANGE_PASSWORD;
+        }
+        else if (cmd.equals("HOST_PORT")) {
+            return Command.HOST_PORT;
+        }
+        else if (cmd.equals("SIGN_OFF")) {
+            return Command.SIGN_OFF;
+        }
+        else if (cmd.equals("ALIVE")) {
+            return Command.ALIVE;
+        }
+            else if (cmd.equals("GET_GAME_IDENTS")) {
+            return Command.GET_GAME_IDENTS;
+        }
+        else if (cmd.equals("RANDOM_PARTICIPANT_HOST_PORT")) {
+            return Command.RANDOM_PARTICIPANT_HOST_PORT;
+        }
+        else if (cmd.equals("PARTICIPANT_HOST_PORT")) {
+            return Command.PARTICIPANT_HOST_PORT;
+        }
+        else {
+            return null;
+        }
+    }
 }
